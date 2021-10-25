5a1a187bd1c7be70391062523f349e8056107a3a
Alexia-Passos <alexia.passos@accurate.com.br>
2021-10-25 01:40:52 -0300

    change routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,9 +4,9 @@ const userController = require('../controllers/userController');
 
 //Por default as rotas já estão definidas como 'http://localhost:3000/users'
 router.get('/', userController.getUsers); //Buscar todos os usuários
-router.get('/:id', userController.getById); //Busca por um usuário
+router.get('/id', userController.getById); //Busca por um usuário
 router.post('/registration', userController.createUser); //Cria um novo usuário
-router.put('/change/:id', userController.alterUser); //Alterar perfil usuário
-router.delete('/delete/:id', userController.deleteUser); //deleta uma conta de usuário
+router.put('/change/id', userController.alterUser); //Alterar perfil usuário
+router.delete('/delete/id', userController.deleteUser); //deleta uma conta de usuário
 
 module.exports = router;
--- routes/workRouter.js
+++ routes/workRouter.js
@@ -4,9 +4,9 @@ const workController = require('../controllers/workController');
 
 //Por default as rotas já estão definidas como 'http://localhost:3000/works'
 router.get('/', workController.getWorks); //Buscar todos os trabalhos
-router.get('/:id', workController.getById); //Busca por um trabalho
+router.get('/id', workController.getById); //Busca por um trabalho
 router.post('/registration', workController.createWork); //Cria um novo trabalho
-router.put('/change/:id', workController.alterWork); //Alterar dados do trabalho
-router.delete('/delete/:id', workController.deleteWork); //deleta um trabalho
+router.put('/change/id', workController.alterWork); //Alterar dados do trabalho
+router.delete('/delete/id', workController.deleteWork); //deleta um trabalho
 
 module.exports = router;
